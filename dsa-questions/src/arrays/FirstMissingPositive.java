581f4e6d465cdfb863d928c007d790402623cfdb
ayaan604 <ayaan604shaikh@gmail.com>
2025-07-27 11:41:20 +0530

    Added description for FirstMissingPositive q

--- dsa-questions/src/arrays/FirstMissingPositive.java
+++ dsa-questions/src/arrays/FirstMissingPositive.java
@@ -1,5 +1,11 @@
 package arrays;
 
+/*
+ Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+ You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+ */
+
 public class FirstMissingPositive {
 	public static void main(String[] args) {
 		FirstMissingPositive main = new FirstMissingPositive();
